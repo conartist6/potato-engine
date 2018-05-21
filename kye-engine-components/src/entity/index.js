7e66adce558dae1bfb0ae50f4a5133bd98ea7ed9
Conrad Buck <conartist6@gmail.com>
2018-05-20 22:49:04 -0400

    fix missing lib

--- kye-engine-components/src/entity/index.js
+++ kye-engine-components/src/entity/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import cx from 'classnames'; // :'
 import dasherize from 'dasherize';
 import { Set } from 'immutable';
-import e from 'kye-engine/entities';
+import e from 'kye-engine/lib/entities';
 
 import './entity.css';
 

2ecf2354c6f711b2efe07339408c176fe0697dff
Conrad Buck <conartist6@gmail.com>
2018-06-11 14:14:46 -0700

    link with much improved lerna-d esdoc2-react

--- docs/build.js
+++ docs/build.js
@@ -1,42 +1,13 @@
-const esdoc = require('esdoc').default;
+const esdoc = require('esdoc2').default;
 
 function makeConfig(pkg) {
   return {
-    source: `../${pkg}/src`,
     destination: `./build/${pkg}`,
-    package: `../${pkg}/package.json`,
-    index: `../${pkg}/README.md`,
+    root: `../packages/${pkg}`,
+    index: `../packages/${pkg}/README.md`,
     plugins: [
       {
-        name: 'esdoc-ecmascript-proposal-plugin',
-        option: {
-          classProperties: true,
-          objectRestSpread: true,
-          dynamicImport: true,
-        },
-      },
-      {
-        name: 'esdoc-accessor-plugin',
-      },
-      {
-        name: 'esdoc-external-ecmascript-plugin',
-      },
-      {
-        name: 'esdoc-external-nodejs-plugin',
-      },
-      {
-        name: 'esdoc-lint-plugin',
-      },
-      {
-        name: 'esdoc-publish-react-plugin',
-        option: {
-          stripPackageName: false,
-          replaces: [
-            { from: '^src/', to: 'kye-engine/lib/' },
-            { from: '\\.js$', to: '' },
-            { from: '/index$', to: '' },
-          ],
-        },
+        name: 'esdoc2-react-standard-plugin',
       },
     ],
   };
